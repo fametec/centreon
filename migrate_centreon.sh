ec75ae640127040da7f76484a4d858de5c83bd10
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-11-30 21:01:00 -0300

    dump.sql

--- migrate_centreon.sh
+++ migrate_centreon.sh
@@ -3,7 +3,7 @@
 # Migrar Centreon 18.10 para outro Host
 #
 
-IP_New_Centreon='http://159.65.37.229'
+IP_New_Centreon='159.65.37.229'
 
 
 
@@ -15,22 +15,28 @@ rsync -avz --exclude centcore/ logs/ /var/lib/centreon root@$IP_New_Centreon:/va
 rsync -avz /var/spool/centreon/.ssh root@$IP_New_Centreon:/var/spool/centreon
 
 
+# Dump Database
+mysqldump -A > /tmp/dump.sql
+
+rsync -avz /tmp/dump.sql root@$IP_New_Centreon:/tmp
+ssh root@root@$IP_New_Centreon "mysql < /tmp/dump.sql.gz"
+
 # Stop mysqld
-ssh root@$IP_New_Centreon "service mysql stop"
+#ssh root@$IP_New_Centreon "service mysql stop"
 
 # Synchronize data
-rsync -avz /var/lib/mysql/ root@$IP_New_Centreon:/var/lib/mysql/
+#rsync -avz /var/lib/mysql/ root@$IP_New_Centreon:/var/lib/mysql/
 
 # If you migrate your DMBS from 5.x to 10.x, it’s necessary to execute this command on the new server :
 MVER=`ssh root@$IP_New_Centreon "mysqld -V | grep Ver | cut -d ' ' -f 4 | cut -d . -f 1"`
-if [ $MVER -eq "5" ]
+if [ $MVER -eq 5 ]
 then
   ssh root@$IP_New_Centreon "mysql_upgrade"
 fi
 
 
 # Start the mysqld process on the new server:
-systemctl start mysqld
+#systemctl start mysqld
 
 # Upgrading Centreon
 ssh root@$IP_New_Centreon "mv /usr/share/centreon/installDir/install* /usr/share/centreon/www/install"
