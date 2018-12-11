d4d4b350feea43b9e6448927d922a10cf86e44ec
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-12-11 16:38:03 -0300

    curl

--- install_centreon.sh
+++ install_centreon.sh
@@ -91,3 +91,30 @@ systemctl start snmptrapd
 
 
 
+# Pos Install 
+# 1 Welcome to Centreon Setup
+#curl -d action="form_step1" -d next="Next" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 2 Dependency check up
+#curl -d action="form_step2" -d next="Next" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 3 Monitoring engine information
+#curl -d action="form_step3" -d submit="Next" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 4 Broker module information
+#curl -d action="form_step4" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 5 Admin information
+#curl -d action="form_step5" -d admin_password='q1w2Q!W@' -d confirm_password='q1w2Q!W@' -d firstname="Admin" -d lastname="Centreon" -d email="root@localhost" -d next="Next" http://localhost/centreon/install/install.php 
+
+# 6 Database information
+#curl -d action="form_step6" -d address="localhost" -d port="3306" -d root_password='' -d db_configuration="centreon" -d db_storage="centreon_storage" -d db_user="centreon" -d db_password='q1w2Q!W@' -d db_password_confirm='q1w2Q!W@' -d next="Next" http://localhost/centreon/install/install.php
+
+# 7 Installation
+#curl -d action="form_step7" -d next="Next" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 8 Modules installation
+#curl -d installModules="Install" http://localhost/centreon/install/install.php
+#curl -d next="Next" -d submit="Next" http://localhost/centreon/install/install.php
+
+# 9 
