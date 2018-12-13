4d406ca4cad23d849281808a5969aebd7e45027a
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-12-13 14:34:44 -0300

    commit-Thu Dec 13 14:34:44 -03 2018

--- migrate_centreon.sh
+++ migrate_centreon.sh
@@ -31,16 +31,25 @@ rsync -avz /var/spool/centreon/.ssh root@$IP_New_Centreon:/var/spool/centreon
 #rsync -avz /usr/lib/centreon/plugins root@$IP_Nes_Centreon:/usr/lib/centreon
 
 
-## Dump Database
-mysqldump --skip-add-drop-database --databases centreon > /tmp/dump_centreon.sql
-rsync -avz /tmp/dump_centreon.sql root@$IP_New_Centreon:/tmp
-ssh root@root@$IP_New_Centreon "mysql < /tmp/dump_centreon.sql"
-read
+## Dump Database centreon
+mysqldump --skip-add-drop-database --databases centreon > /tmp/dump_centreon.sql.gz 
+rsync -avz /tmp/dump_centreon.sql.gz root@$IP_New_Centreon:/tmp
 
+## Dump Database centreon_storage
 mysqldump --skip-add-drop-database --databases centreon_storage > /tmp/dump_centreon_storage.sql
 rsync -avz /tmp/dump_centreon_storage.sql root@$IP_New_Centreon:/tmp
-ssh root@root@$IP_New_Centreon "mysql < /tmp/dump_centreon_storage.sql"
-read
+
+## Script restore databases
+cat <<EOF >> /tmp/script1.sh
+#!/bin/bash
+#Restore databases
+mysql -v < /tmp/dump_centreon.sql
+mysql -v < /tmp/dump_centreon_storage.sql
+EOF
+
+rsync -avz /tmp/script1.sh root@$IP_New_Centreon:/tmp
+ssh root@$IP_New_Centreon "sh /tmp/script1.sh"
+
 
 # Stop mysqld
 #ssh root@$IP_New_Centreon "service mysql stop"
@@ -60,17 +69,14 @@ read
 #systemctl start mysqld
 
 # Upgrading Centreon
-cat << EOF >> /tmp/script1.sh
+cat << EOF >> /tmp/script2.sh
 DIR=\$(ls -1 /usr/share/centreon/installDir)
 mv /usr/share/centreon/installDir/\$DIR/ /usr/share/centreon/www/install
 EOF
 
-rsync -avz /tmp/script1.sh root@$IP_New_Centreon:/tmp
-ssh root@$IP_New_Centreon "sh /tmp/script1.sh"
 
-
-
-ssh root@root@$IP_New_Centreon "mysql -v < /tmp/dump.sql"
+rsync -avz /tmp/script2.sh root@$IP_New_Centreon:/tmp
+ssh root@$IP_New_Centreon "sh /tmp/script2.sh"
 
 # Fonte: 
 # https://documentation.centreon.com/docs/centreon/en/latest/migration/upgradetoCentreon18.10.html
