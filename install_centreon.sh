480189c8f43a4c8c4c1a10aad47e3ff99020ab39
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-12-13 07:51:51 -0300

    Thu Dec 13 07:51:51 -03 2018

--- install_centreon.sh
+++ install_centreon.sh
@@ -93,22 +93,22 @@ systemctl start snmptrapd
 
 # Pos Install 
 # 1 Welcome to Centreon Setup
-curl http://localhost/centreon/install/steps/step.php?action=nextStep
+#curl http://localhost/centreon/install/steps/step.php?action=nextStep
 
 # 2 Dependency check up
-curl http://localhost/centreon/install/steps/step.php?action=nextStep
+#curl http://localhost/centreon/install/steps/step.php?action=nextStep
 
 # 3 Monitoring engine information
-curl -d install_dir_engine='/usr/share/centreon-engine' -d centreon_engine_stats_binary='/usr/sbin/centenginestats' -d monitoring_var_lib='/var/lib/centreon-engine' -d centreon_engine_connectors='/usr/lib64/centreon-connector' -d centreon_engine_lib='/usr/lib64/centreon-engine' -d centreonplugins='/usr/lib/centreon/plugins/' http://localhost/centreon/install/steps/process/process_step3.php
+#curl -d install_dir_engine='/usr/share/centreon-engine' -d centreon_engine_stats_binary='/usr/sbin/centenginestats' -d monitoring_var_lib='/var/lib/centreon-engine' -d centreon_engine_connectors='/usr/lib64/centreon-connector' -d centreon_engine_lib='/usr/lib64/centreon-engine' -d centreonplugins='/usr/lib/centreon/plugins/' http://localhost/centreon/install/steps/process/process_step3.php
 
 # 4 Broker module information
-curl -d centreonbroker_etc='/etc/centreon-broker' -d centreonbroker_cbmod='/usr/lib64/nagios/cbmod.so' -d centreonbroker_log='/var/log/centreon-broker' -d centreonbroker_varlib='/var/lib/centreon-broker' -d centreonbroker_lib='/usr/share/centreon/lib/centreon-broker' http://localhost/centreon/install/steps/process/process_step4.php
+#curl -d centreonbroker_etc='/etc/centreon-broker' -d centreonbroker_cbmod='/usr/lib64/nagios/cbmod.so' -d centreonbroker_log='/var/log/centreon-broker' -d centreonbroker_varlib='/var/lib/centreon-broker' -d centreonbroker_lib='/usr/share/centreon/lib/centreon-broker' http://localhost/centreon/install/steps/process/process_step4.php
 
 # 5 Admin information
-curl -d admin_password='q1w2Q!W@' -d confirm_password='q1w2Q!W@' -d firstname="Admin" -d lastname="Centreon" -d email="root@localhost" http://localhost/centreon/install/steps/process/process_step5.php 
+#curl -d admin_password='q1w2Q!W@' -d confirm_password='q1w2Q!W@' -d firstname="Admin" -d lastname="Centreon" -d email="root@localhost" http://localhost/centreon/install/steps/process/process_step5.php 
 
 # 6 Database information
-curl -d address='localhost' -d port='3306' --form root_password='' -d db_configuration='centreon' -d db_storage='centreon_storage' -d db_user='centreon' -d db_password='q1w2Q!W@' -d db_password_confirm='q1w2Q!W@' http://localhost/centreon/install/steps/process/process_step6.php
+#curl -d address='localhost' -d port='3306' --form root_password='' -d db_configuration='centreon' -d db_storage='centreon_storage' -d db_user='centreon' -d db_password='q1w2Q!W@' -d db_password_confirm='q1w2Q!W@' http://localhost/centreon/install/steps/process/process_step6.php
 
 # 7 Installation
 #curl http://localhost/centreon/install/steps/step.php?action=nextStep
